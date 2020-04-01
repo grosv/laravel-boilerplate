1529726da2ffcff12f14407683ac906d1c845fff
Ed Grosvenor <ed.grosvenor@wickedviral.com>
2020-04-01 15:51:25 -0500

    WIP

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,14 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TemporaryController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
+/* TEMPORARY ROUTES - DELETE BEFORE GOING LIVE */
+Route::get('/tmp', [TemporaryController::class, 'render'])->name('tmp');
+/* END TEMPORARY ROUTES */
 Auth::routes();
 Route::get('/', [WelcomeController::class, 'render'])->name('welcome');
 Route::get('/home', [HomeController::class, 'render'])->name('home');
